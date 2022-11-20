7fb4672f493d7be3d294e4701ab160a6e5df8ff2
blascojavi <57035019+blascojavi@users.noreply.github.com>
2022-11-20 14:08:56 +0100

    Add files via upload

---
+++ Prueba/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.panelCheckBoxes = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Handlers = New System.Windows.Forms.Button()
+        Me.Imagen = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.BorrarImagen = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(91, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "MessageBox"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'panelCheckBoxes
+        '
+        Me.panelCheckBoxes.Location = New System.Drawing.Point(39, 70)
+        Me.panelCheckBoxes.Name = "panelCheckBoxes"
+        Me.panelCheckBoxes.Size = New System.Drawing.Size(444, 341)
+        Me.panelCheckBoxes.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(93, 429)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(111, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Marcar Todo"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(290, 429)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(109, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Desmarcar Todo"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(118, 12)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Sonido"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(210, 12)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "Do While"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Handlers
+        '
+        Me.Handlers.Location = New System.Drawing.Point(509, 70)
+        Me.Handlers.Name = "Handlers"
+        Me.Handlers.Size = New System.Drawing.Size(75, 23)
+        Me.Handlers.TabIndex = 6
+        Me.Handlers.Text = "Handlers"
+        Me.Handlers.UseVisualStyleBackColor = True
+        '
+        'Imagen
+        '
+        Me.Imagen.Location = New System.Drawing.Point(52, 596)
+        Me.Imagen.Name = "Imagen"
+        Me.Imagen.Size = New System.Drawing.Size(84, 23)
+        Me.Imagen.TabIndex = 7
+        Me.Imagen.Text = "Cargar Imagen"
+        Me.Imagen.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(39, 498)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(136, 92)
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'BorrarImagen
+        '
+        Me.BorrarImagen.Location = New System.Drawing.Point(52, 625)
+        Me.BorrarImagen.Name = "BorrarImagen"
+        Me.BorrarImagen.Size = New System.Drawing.Size(84, 23)
+        Me.BorrarImagen.TabIndex = 9
+        Me.BorrarImagen.Text = "Borrar Imagen"
+        Me.BorrarImagen.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(654, 648)
+        Me.Controls.Add(Me.BorrarImagen)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Imagen)
+        Me.Controls.Add(Me.Handlers)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.panelCheckBoxes)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents panelCheckBoxes As Panel
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Handlers As Button
+    Friend WithEvents Imagen As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents BorrarImagen As Button
+End Class
---
+++ Prueba/Form1.vb
@@ -0,0 +1,127 @@
+﻿Public Class Form1
+    Dim anchoCheckBox As Integer = 45
+
+
+
+    Private Sub centerForm() ''Centrar Formulario en pantalla
+
+        Dim r = Screen.PrimaryScreen.WorkingArea
+        Dim x = r.Width - Me.Width
+        Dim y = r.Height - Me.Height
+
+        x = CInt(x / 2)
+        y = CInt(y / 2)
+        Me.StartPosition = FormStartPosition.Manual
+        Me.Location = New Point(x, y)
+
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Ha ocurrido un error", "error")
+        centerForm()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim numeroCasilla = 1
+
+        Dim left As Integer = 1
+        Dim top As Integer = 1
+        centerForm()
+        ' Generamos los checkboxes en tiempo de ejecucion 
+        For i = 0 To 6
+            For j = 0 To 6
+                Dim checkBox As New CheckBox
+                checkBox.Text = ":" & numeroCasilla
+                checkBox.RightToLeft = RightToLeft.Yes
+                checkBox.Anchor = AnchorStyles.None
+                checkBox.Width = anchoCheckBox
+                checkBox.Left = left
+                checkBox.Top = top
+                checkBox.Font = New Font(Me.Font, FontStyle.Bold)
+                panelCheckBoxes.Controls.Add(checkBox)
+                numeroCasilla = numeroCasilla + 1
+                left = left + 62
+            Next j
+            left = 0
+            top = top + 42
+        Next i
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Recorre todos los checkboxes y les pone el check
+        For i = 0 To panelCheckBoxes.Controls.Count - 1
+            Dim checkbox As CheckBox
+            checkbox = panelCheckBoxes.Controls(i)
+            checkbox.Checked = True
+        Next
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' Recorre todos los checkboxes y les quita el check
+        For i = 0 To panelCheckBoxes.Controls.Count - 1
+            Dim checkbox As CheckBox
+            checkbox = panelCheckBoxes.Controls(i)
+            checkbox.Checked = False
+        Next
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim startsSoundPlayer = New System.Media.SoundPlayer("C:\Windows\media\chord.wav")
+        startsSoundPlayer.Play()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim i As Integer = 0
+        Do
+
+            MsgBox("El valor es:" & i)
+            i += 1
+        Loop While i < 5
+    End Sub
+
+    Private Sub Imagen_Click(sender As Object, e As EventArgs) Handles Imagen.Click
+        'Mostrar el cuadro de diálogo Abrir archivo. Si el usuario hace clic en Aceptar, cargue la
+        'imagen que el usuario eligió.
+        If (OpenFileDialog1.ShowDialog() = DialogResult.OK) Then
+            PictureBox1.Load(OpenFileDialog1.FileName)
+
+        End If
+    End Sub
+
+    Private Sub BorrarImagen_Click(sender As Object, e As EventArgs) Handles BorrarImagen.Click
+        'clear the picture 
+        PictureBox1.Image = Nothing
+        'checkWinner()
+    End Sub
+
+    Public Sub checkWinner()
+        'No Funciona
+        Dim position3(1, 2, 3)
+        Dim position3_1(4, 5, 6)
+        Dim position3_2(7, 8, 9)
+        Dim position3_3(1, 5, 9)
+        Dim position3_4(7, 5, 3)
+
+        '
+        'Dim position4 As ArrayList =
+        'Dim position5 As ArrayList = 
+        Dim checkWinnerArray As Array = {1, 2, 4}
+
+        If UBound(checkWinnerArray) > UBound(position3) Then
+            MessageBox.Show("Ganador", "Winner")
+        Else
+            MessageBox.Show("Perdedor", "Loser")
+        End If
+
+
+
+
+
+
+
+    End Sub
+
+
+
+End Class
